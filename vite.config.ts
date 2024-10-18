b6dd8cd9c7258d7f85d04acac09319a8ba880c99
stasrusnak <stasrusnak@gmail.com>
2024-10-18 03:49:18 +0300

    update env

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc';
 
-
-export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  return {
-    base: env.VITE_BASE_URL,
-    plugins: [react()],
-  }
+export default defineConfig({
+  base: "/honey-team/",
+  plugins: [react()],
 })
